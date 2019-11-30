992c53323b3444e08a591916b9642ac58c62f453
Elizabeth Larson <eml2017@yahoo.com>
2019-11-30 14:58:16 -0800

    piano sounds

--- src/PianoController.java
+++ src/PianoController.java
@@ -6,10 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.InputStream;
 
-// Sources used to play the sound file
-// I made the sound file in the program Audacity, so nothing to cite for that yet
+// Sound files downloaded from:
+// https://freesound.org/people/Tesabob2001/packs/12995/
+
+// Sources used to play the sound file:
 // https://alvinalexander.com/java/java-audio-example-java-au-play-sound
-//      ^ playing the .wav sound file found in src folder
 
 public class PianoController {
     private PianoModel pianoModel;
@@ -20,17 +21,16 @@ public class PianoController {
         PianoController controller = new PianoController(pianoModel);
     }
 
+    // C3 -> C4 working
     public PianoController(PianoModel pianoModel) {
         this.pianoModel = pianoModel;
         this.pianoView = new PianoView(this);
 
-        pianoView.c1Key.addActionListener(new ActionListener() {
+        pianoView.c3Key.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JButton button = (JButton) e.getSource();
                 try {
-                    System.out.println("The C1 key was pushed");
-                    InputStream inputStream = getClass().getResourceAsStream("tone.wav");
+                    InputStream inputStream = getClass().getResourceAsStream("c3.wav");
                     AudioStream audioStream = new AudioStream(inputStream);
                     AudioPlayer.player.start(audioStream);
                 } catch (Exception ex) {
@@ -39,13 +39,154 @@ public class PianoController {
             }
         });
 
-        pianoView.d1Key.addActionListener(new ActionListener() {
+        pianoView.d3Key.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JButton button = (JButton) e.getSource();
                 try {
-                    System.out.println("The D1 key was pushed");
-                    InputStream inputStream = getClass().getResourceAsStream("tone.wav");
+                    InputStream inputStream = getClass().getResourceAsStream("d3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.e3Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("e3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.f3Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("f3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.g3Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("g3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.a3Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("a3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.b3Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("b3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.c4Key.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("c4.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.c3SharpKey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("c#3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.d3SharpKey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("d#3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.f3SharpKey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("f#3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.g3SharpKey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("g#3.wav");
+                    AudioStream audioStream = new AudioStream(inputStream);
+                    AudioPlayer.player.start(audioStream);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        pianoView.a3SharpKey.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    InputStream inputStream = getClass().getResourceAsStream("a#3.wav");
                     AudioStream audioStream = new AudioStream(inputStream);
                     AudioPlayer.player.start(audioStream);
                 } catch (Exception ex) {
--- src/PianoView.java
+++ src/PianoView.java
@@ -9,20 +9,20 @@ public class PianoView extends JFrame {
     // Octave 0 (A0 and B0)
     // Octave 1 (<note name><octave number>Key)
     // White keys
-    protected JButton c1Key;
-    protected JButton d1Key;
-    protected JButton e1Key;
-    protected JButton f1Key;
-    protected JButton g1Key;
-    protected JButton a1Key;
-    protected JButton b1Key;
-    protected JButton c2Key;
+    protected JButton c3Key;
+    protected JButton d3Key;
+    protected JButton e3Key;
+    protected JButton f3Key;
+    protected JButton g3Key;
+    protected JButton a3Key;
+    protected JButton b3Key;
+    protected JButton c4Key;
     // Black keys
-    protected JButton c1SharpKey;
-    protected JButton d1SharpKey;
-    protected JButton f1SharpKey;
-    protected JButton g1SharpKey;
-    protected JButton a1SharpKey;
+    protected JButton c3SharpKey;
+    protected JButton d3SharpKey;
+    protected JButton f3SharpKey;
+    protected JButton g3SharpKey;
+    protected JButton a3SharpKey;
     // Octave 2
     // Octave 3
     // Octave 4
@@ -69,51 +69,51 @@ public class PianoView extends JFrame {
         whitePianoKeys.setLayout(new GridLayout(1, 8));
 
         // Set background found by playing around with IntelliJ's autocomplete
-        c1SharpKey = new JButton("C1#");
-        c1SharpKey.setBackground(Color.BLACK);
-        c1SharpKey.setForeground(Color.WHITE);
-        blackPianoKeys.add(c1SharpKey);
-        d1SharpKey = new JButton("D1#");
-        d1SharpKey.setBackground(Color.BLACK);
-        d1SharpKey.setForeground(Color.WHITE);
-        blackPianoKeys.add(d1SharpKey);
-        f1SharpKey = new JButton("F1#");
-        f1SharpKey.setBackground(Color.BLACK);
-        f1SharpKey.setForeground(Color.WHITE);
-        blackPianoKeys.add(f1SharpKey);
-        g1SharpKey = new JButton("G1#");
-        g1SharpKey.setBackground(Color.BLACK);
-        g1SharpKey.setForeground(Color.WHITE);
-        blackPianoKeys.add(g1SharpKey);
-        a1SharpKey = new JButton("A1#");
-        a1SharpKey.setBackground(Color.BLACK);
-        a1SharpKey.setForeground(Color.WHITE);
-        blackPianoKeys.add(a1SharpKey);
+        c3SharpKey = new JButton("C3#");
+        c3SharpKey.setBackground(Color.BLACK);
+        c3SharpKey.setForeground(Color.WHITE);
+        blackPianoKeys.add(c3SharpKey);
+        d3SharpKey = new JButton("D3#");
+        d3SharpKey.setBackground(Color.BLACK);
+        d3SharpKey.setForeground(Color.WHITE);
+        blackPianoKeys.add(d3SharpKey);
+        f3SharpKey = new JButton("F3#");
+        f3SharpKey.setBackground(Color.BLACK);
+        f3SharpKey.setForeground(Color.WHITE);
+        blackPianoKeys.add(f3SharpKey);
+        g3SharpKey = new JButton("G3#");
+        g3SharpKey.setBackground(Color.BLACK);
+        g3SharpKey.setForeground(Color.WHITE);
+        blackPianoKeys.add(g3SharpKey);
+        a3SharpKey = new JButton("A3#");
+        a3SharpKey.setBackground(Color.BLACK);
+        a3SharpKey.setForeground(Color.WHITE);
+        blackPianoKeys.add(a3SharpKey);
 
-        c1Key = new JButton("C1");
-        c1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(c1Key);
-        d1Key = new JButton("D1");
-        d1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(d1Key);
-        e1Key = new JButton("E1");
-        e1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(e1Key);
-        f1Key = new JButton("F1");
-        f1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(f1Key);
-        g1Key = new JButton("G1");
-        g1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(g1Key);
-        a1Key = new JButton("A1");
-        a1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(a1Key);
-        b1Key = new JButton("B1");
-        b1Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(b1Key);
-        c2Key = new JButton("C2");
-        c2Key.setBackground(Color.WHITE);
-        whitePianoKeys.add(c2Key);
+        c3Key = new JButton("C3");
+        c3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(c3Key);
+        d3Key = new JButton("D3");
+        d3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(d3Key);
+        e3Key = new JButton("E3");
+        e3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(e3Key);
+        f3Key = new JButton("F3");
+        f3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(f3Key);
+        g3Key = new JButton("G3");
+        g3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(g3Key);
+        a3Key = new JButton("A3");
+        a3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(a3Key);
+        b3Key = new JButton("B3");
+        b3Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(b3Key);
+        c4Key = new JButton("C4");
+        c4Key.setBackground(Color.WHITE);
+        whitePianoKeys.add(c4Key);
 
         contentPanel.add(sliderPanel);
         contentPanel.add(blackPianoKeys);
